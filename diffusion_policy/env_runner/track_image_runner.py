25ebcb801bedc3f12717651cfdd3b18dbdfad360
Ice-mao <648558423@qq.com>
2025-05-27 19:19:19 +0800

    git update for eval and train

--- diffusion_policy/env_runner/track_image_runner.py
+++ diffusion_policy/env_runner/track_image_runner.py
@@ -17,128 +17,19 @@ from diffusion_policy.policy.base_image_policy import BaseImagePolicy
 from diffusion_policy.common.pytorch_util import dict_apply
 from diffusion_policy.env_runner.base_image_runner import BaseImageRunner
 
-class PushTImageRunner(BaseImageRunner):
+class TrackImageRunner(BaseImageRunner):
     def __init__(self,
             output_dir,
-            n_train=10,
-            n_train_vis=3,
-            train_start_seed=0,
-            n_test=22,
-            n_test_vis=6,
-            legacy_test=False,
-            test_start_seed=10000,
             max_steps=200,
             n_obs_steps=8,
             n_action_steps=8,
-            fps=10,
-            crf=22,
-            render_size=96,
-            past_action=False,
-            tqdm_interval_sec=5.0,
-            n_envs=None
         ):
         super().__init__(output_dir)
-        if n_envs is None:
-            n_envs = n_train + n_test
-
-        steps_per_render = max(10 // fps, 1)
-        def env_fn():
-            return MultiStepWrapper(
-                VideoRecordingWrapper(
-                    PushTImageEnv(
-                        legacy=legacy_test,
-                        render_size=render_size
-                    ),
-                    video_recoder=VideoRecorder.create_h264(
-                        fps=fps,
-                        codec='h264',
-                        input_pix_fmt='rgb24',
-                        crf=crf,
-                        thread_type='FRAME',
-                        thread_count=1
-                    ),
-                    file_path=None,
-                    steps_per_render=steps_per_render
-                ),
-                n_obs_steps=n_obs_steps,
-                n_action_steps=n_action_steps,
-                max_episode_steps=max_steps
-            )
-
-        env_fns = [env_fn] * n_envs
-        env_seeds = list()
-        env_prefixs = list()
-        env_init_fn_dills = list()
-        # train
-        for i in range(n_train):
-            seed = train_start_seed + i
-            enable_render = i < n_train_vis
-
-            def init_fn(env, seed=seed, enable_render=enable_render):
-                # setup rendering
-                # video_wrapper
-                assert isinstance(env.env, VideoRecordingWrapper)
-                env.env.video_recoder.stop()
-                env.env.file_path = None
-                if enable_render:
-                    filename = pathlib.Path(output_dir).joinpath(
-                        'media', wv.util.generate_id() + ".mp4")
-                    filename.parent.mkdir(parents=False, exist_ok=True)
-                    filename = str(filename)
-                    env.env.file_path = filename
-
-                # set seed
-                assert isinstance(env, MultiStepWrapper)
-                env.seed(seed)
-            
-            env_seeds.append(seed)
-            env_prefixs.append('train/')
-            env_init_fn_dills.append(dill.dumps(init_fn))
-
-        # test
-        for i in range(n_test):
-            seed = test_start_seed + i
-            enable_render = i < n_test_vis
-
-            def init_fn(env, seed=seed, enable_render=enable_render):
-                # setup rendering
-                # video_wrapper
-                assert isinstance(env.env, VideoRecordingWrapper)
-                env.env.video_recoder.stop()
-                env.env.file_path = None
-                if enable_render:
-                    filename = pathlib.Path(output_dir).joinpath(
-                        'media', wv.util.generate_id() + ".mp4")
-                    filename.parent.mkdir(parents=False, exist_ok=True)
-                    filename = str(filename)
-                    env.env.file_path = filename
-
-                # set seed
-                assert isinstance(env, MultiStepWrapper)
-                env.seed(seed)
-            
-            env_seeds.append(seed)
-            env_prefixs.append('test/')
-            env_init_fn_dills.append(dill.dumps(init_fn))
-
-        env = AsyncVectorEnv(env_fns)
-
-        # test env
-        # env.reset(seed=env_seeds)
-        # x = env.step(env.action_space.sample())
-        # imgs = env.call('render')
-        # import pdb; pdb.set_trace()
 
         self.env = env
-        self.env_fns = env_fns
-        self.env_seeds = env_seeds
-        self.env_prefixs = env_prefixs
-        self.env_init_fn_dills = env_init_fn_dills
-        self.fps = fps
         self.crf = crf
         self.n_obs_steps = n_obs_steps
         self.n_action_steps = n_action_steps
-        self.past_action = past_action
         self.max_steps = max_steps
         self.tqdm_interval_sec = tqdm_interval_sec
     
@@ -148,75 +39,50 @@ class PushTImageRunner(BaseImageRunner):
         env = self.env
 
         # plan for rollout
-        n_envs = len(self.env_fns)
-        n_inits = len(self.env_init_fn_dills)
-        n_chunks = math.ceil(n_inits / n_envs)
+        # n_envs = len(self.env_fns)
+        # n_inits = len(self.env_init_fn_dills)
+        # n_chunks = math.ceil(n_inits / n_envs)
 
-        # allocate data
-        all_video_paths = [None] * n_inits
-        all_rewards = [None] * n_inits
+        # start rollout
+        obs, _ = env.reset()
+        policy.reset()
 
-        for chunk_idx in range(n_chunks):
-            start = chunk_idx * n_envs
-            end = min(n_inits, start + n_envs)
-            this_global_slice = slice(start, end)
-            this_n_active_envs = end - start
-            this_local_slice = slice(0,this_n_active_envs)
+        pbar = tqdm.tqdm(total=self.max_steps, desc=f"Eval PushtImageRunner {chunk_idx+1}/{n_chunks}", 
+            leave=False, mininterval=self.tqdm_interval_sec)
+        done = False
+        while not done:
+            # create obs dict
+            np_obs_dict = dict(obs)
+            # if self.past_action and (past_action is not None):
+            #     # TODO: not tested
+            #     np_obs_dict['past_action'] = past_action[
+            #         :,-(self.n_obs_steps-1):].astype(np.float32)
             
-            this_init_fns = self.env_init_fn_dills[this_global_slice]
-            n_diff = n_envs - len(this_init_fns)
-            if n_diff > 0:
-                this_init_fns.extend([self.env_init_fn_dills[0]]*n_diff)
-            assert len(this_init_fns) == n_envs
+            # device transfer
+            obs_dict = dict_apply(np_obs_dict, 
+                lambda x: torch.from_numpy(x).to(
+                    device=device))
 
-            # init envs
-            env.call_each('run_dill_function', 
-                args_list=[(x,) for x in this_init_fns])
+            # run policy
+            with torch.no_grad():
+                action_dict = policy.predict_action(obs_dict)
 
-            # start rollout
-            obs = env.reset()
-            past_action = None
-            policy.reset()
+            # device_transfer
+            np_action_dict = dict_apply(action_dict,
+                lambda x: x.detach().to('cpu').numpy())
 
-            pbar = tqdm.tqdm(total=self.max_steps, desc=f"Eval PushtImageRunner {chunk_idx+1}/{n_chunks}", 
-                leave=False, mininterval=self.tqdm_interval_sec)
-            done = False
-            while not done:
-                # create obs dict
-                np_obs_dict = dict(obs)
-                if self.past_action and (past_action is not None):
-                    # TODO: not tested
-                    np_obs_dict['past_action'] = past_action[
-                        :,-(self.n_obs_steps-1):].astype(np.float32)
-                
-                # device transfer
-                obs_dict = dict_apply(np_obs_dict, 
-                    lambda x: torch.from_numpy(x).to(
-                        device=device))
+            action = np_action_dict['action']
 
-                # run policy
-                with torch.no_grad():
-                    action_dict = policy.predict_action(obs_dict)
+            # step env
+            obs, reward, done, info = env.step(action)
+            done = np.all(done)
+            past_action = action
 
-                # device_transfer
-                np_action_dict = dict_apply(action_dict,
-                    lambda x: x.detach().to('cpu').numpy())
+            # update pbar
+            pbar.update(action.shape[1])
+        pbar.close()
 
-                action = np_action_dict['action']
-
-                # step env
-                obs, reward, done, info = env.step(action)
-                done = np.all(done)
-                past_action = action
-
-                # update pbar
-                pbar.update(action.shape[1])
-            pbar.close()
-
-            all_video_paths[this_global_slice] = env.render()[this_local_slice]
-            all_rewards[this_global_slice] = env.call('get_attr', 'reward')[this_local_slice]
-        # clear out video buffer
-        _ = env.reset()
+        all_rewards[this_global_slice] = env.call('get_attr', 'reward')[this_local_slice]
 
         # log
         max_rewards = collections.defaultdict(list)
